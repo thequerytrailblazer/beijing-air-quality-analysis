523a2c58a35ed73102eaa93fd6d3c8a71a217b19
thequerytrailblazer <hisyam.athaya@ui.ac.id>
2026-08-24 23:24:30 +0700

    updating footer

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -172,5 +172,5 @@ with col_right:
 st.divider()
 st.caption(
     "Data diperoleh dari repositori GitHub [HTI (marceloreis/HTI)](https://github.com/marceloreis/HTI) "
-    "berlisensi GPL 3.0 | Copyright © 2026 - Hisyam Athaya"
+    "berlisensi GPL 3.0 | Copyright © 2026 - HA - The Query Trailblazer"
 )
